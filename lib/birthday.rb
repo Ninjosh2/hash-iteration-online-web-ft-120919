ffdf34093cc36700ac099d0f59fcaab77904d3c4
Joshua Sanders <ninjosh2@gmail.com>
2019-12-18 00:51:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,4 +12,4 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age > 12 puts 
+    if age > 12 puts ("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n")
